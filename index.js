659d27a7a373755fe52b6eea1d62f0c0d90b2e04
Brian Faust <hello@basecode.sh>
2022-08-13 04:16:52 +0300

    feat: pass index and samples count to callback

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function mark(label, samples, callback) {
   const [μs, ms, sec] = [10n ** 3n, 10n ** 6n, 10n ** 9n];
   const start = getTime();
   for (let i = 0; i < samples; i++) {
-    let val = callback();
+    let val = callback({ i, samples });
     if (val instanceof Promise) await val;
   }
   const end = getTime();
